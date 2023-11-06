7d0013de46659e63014b0bc80e35b31480eadb19
Roy Wilson <roystetwilson@gmail.com>
2023-11-06 21:13:01 +0000

    Q4 - product of 2 ints

---
+++ 4_Strings/productInt.py
@@ -0,0 +1,6 @@
+# obtain the product of 2 integers
+
+x: int = int(input('Please enter first integer: '))
+y: int = int(input('Please enter secong integer: '))
+
+print(f'The product of {x} multiplied by {y} is {x * y}')
